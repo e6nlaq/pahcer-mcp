e76717083da7c5f89e3f47cf7ee1abe3e5b1fa2a
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-10-02 23:41:29 +0900

    ci

---
+++ test/install.test.ts
@@ -0,0 +1,11 @@
+import {test, expect} from "bun:test";
+import {isInstalledPahcer} from "../src/pahcer";
+
+test.if(process.env.INSTALLED_PAHCER === true)("pahcer is installed", async () => {
+    expect(await isInstalledPahcer()).toBe(true);
+})
+
+test.if(process.env.INSTALLED_PAHCER === undefined)("pahcer is not installed", async () => {
+    expect(await isInstalledPahcer()).toBe(false);
+})
+
---
+++ types.d.ts
@@ -0,0 +1,5 @@
+declare module "bun" {
+  interface Env {
+    INSTALLED_PAHCER?: true|undefined;
+  }
+}
